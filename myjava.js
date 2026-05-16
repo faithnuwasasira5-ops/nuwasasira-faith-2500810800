ec51c1c894defd0e5f3c44cc8aad146e3f067b17
NUWASASIRA FAITH <faithnuwasasira5@gmail.com>
2026-05-16 17:51:46 +0300

    Add files via upload

---
+++ myjava.js
@@ -0,0 +1,9 @@
+alert("refer");
+let btn=document.getElementById("btn");
+if(btn){
+    btn.addEventListener("click", function(){
+        alert("WELCOME TO COASTA RICA RESIDENCY AND PROPERTY");
+    });
+}
+document.getElementsById(".righ-container");
+Section.style.color="pink"
